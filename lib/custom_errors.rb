edd072d528b49e1bfad96a5f760613a2609ddc7b
Loren Cipriano <loren25cipriano@gmail.com>
2020-12-11 11:34:58 -0800

    commit tree 07c58d7d8b48f1aea534fc7bc2a4d2f6e6126c9a

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -1,21 +1,41 @@
 class Person
-  attr_accessor :partner, :name
+  attr_accessor :name, :partner
 
   def initialize(name)
     @name = name
   end
 
+  # def get_married(person)
+  #   self.partner = person
+  #   person.partner = self
+  # end
+
+
   def get_married(person)
     self.partner = person
-    person.partner = self
+    if person.class != Person
+      begin
+        raise PartnerError            #RIASE ERROR
+      rescue PartnerError => error    # RESCUE ERROR -keep code running (pass error) returns => beyonce
+          puts error.message
+      end
+    else
+      person.partner = self
+    end
   end
 
+  # Add the following two lines:
+  class PartnerError < StandardError
+    def message
+      "you must give the get_married method an argument of an instance of the person class!"
+    end
+  end
 end
 
 beyonce = Person.new("Beyonce")
 beyonce.get_married("Jay-Z")
-puts beyonce.name
+puts beyonce.name 
 
 
-
-
+#=> you must give the get_married method an argument of an instance of the person class!
+#=> Beyonce
